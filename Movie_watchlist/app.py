e26fce6d892a407c946473e20d5c9a8450fb38e5
Manuel M <64323226+Manuel-mane@users.noreply.github.com>
2020-09-04 21:50:48 -0400

    Improving the queryings, adding options and including indexing for faster response

--- Movie_watchlist/app.py
+++ Movie_watchlist/app.py
@@ -8,7 +8,8 @@ menu = """Please select one of the following options:
 4) Watch a movie
 5) View watched movies.
 6) Add user to the app
-7) Exit.
+7) Search for a movie
+8) Exit.
 
 Your selection: """
 welcome = "Welcome to the watchlist app!"
@@ -41,6 +42,15 @@ def prompt_show_watched_movies():
     else:
         print("This user has watched no movies")
 
+def prompt_search_movies():
+    search_term = input("Enter the partial movie title: ")
+    movies = database.search_movies(search_term)
+    if movies:
+        print_movie_list("Movies found",movies)
+    else:
+        print("Found no movies for that search term!")
+
+
 
 
 def prompt_watch_movie():
@@ -52,7 +62,7 @@ def prompt_add_user():
     username = input("Username: ")
     database.add_user(username)
 
-while (user_input := input(menu)) != "7":
+while (user_input := input(menu)) != "8":
     if user_input == "1":
         prompt_add_movie()
 
@@ -72,3 +82,9 @@ while (user_input := input(menu)) != "7":
 
     elif user_input == "6":
         prompt_add_user()
+    
+    elif user_input == "7":
+        prompt_search_movies()
+
+    else:
+        print("Invalid input, please try again")
--- Movie_watchlist/database.py
+++ Movie_watchlist/database.py
@@ -16,8 +16,8 @@ CREATE_USER_TABLE = """CREATE TABLE IF NOT EXISTS users(
 CREATE_WATCHED_TABLE = """CREATE TABLE IF NOT EXISTS watched(
     user_username TEXT,
     movie_id INTEGER,
-    FOREIGN KEY(user_username) REFERENCE users(username),
-    FOREIGN KEY(movie_id) REFERENCE movies(id)
+    FOREIGN KEY(user_username) REFERENCES users(username),
+    FOREIGN KEY(movie_id) REFERENCES movies(id)
 );
 """
 
@@ -29,9 +29,11 @@ SELECT_UPCOMING_MOVIES = "SELECT * FROM movies WHERE release_timestamp > ?;"
 SELECT_WATCHED_MOVIES = """SELECT movies.* FROM movies
  JOIN watched ON movies.id = watched.movie_id
  JOIN users ON watched.user_username = users.username
- WHERE user.username = ?;"""
-INSERT_WATCHED_MOVIE = "INSERT INTO watched (user_username, user_id) VALUES (?, ?)"
+ WHERE users.username = ?;"""
+INSERT_WATCHED_MOVIE = "INSERT INTO watched (user_username, movie_id) VALUES (?, ?)"
 SET_MOVIE_WATCHED = "UPDATE movies SET watched = 1 WHERE title = ?;"
+SEARCH_MOVIE = "SELECT * FROM movies WHERE title LIKE ?;"
+CREATE_RELEASE_INDEX = "CREATE INDEX IF NOT EXISTS idx_movies_release ON movies(release_timestamp);"
 
 
 connection = sqlite3.connect("data.db")
@@ -42,6 +44,7 @@ def create_tables():
         connection.execute(CREATE_MOVIE_TABLE)
         connection.execute(CREATE_WATCHED_TABLE)
         connection.execute(CREATE_USER_TABLE)
+        connection.execute(CREATE_RELEASE_INDEX)
 
 def add_user(username):
     with connection:
@@ -63,8 +66,14 @@ def get_movies(upcoming=False):
             cursor.execute(SELECT_ALL_MOVIES)
         return cursor.fetchall()
 
+def search_movies(search_term):
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(SEARCH_MOVIE, (f"%{search_term}%",))
+        return cursor.fetchall()
 
-def watch_movies(username, title):
+
+def watch_movies(username, movie_id):
     with connection:        
         connection.execute(INSERT_WATCHED_MOVIE, (username, movie_id))
 
